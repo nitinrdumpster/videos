6637d102d610526940010ddba5e957b9490ba823
Nitin Ranganath <nitinranganath@gmail.com>
2020-05-09 16:56:34 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Searchbar from "./Searchbar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+const KEY = "AIzaSyBJJGFDq8OZ8MH7iFnfJVSjzABRjTXwPz0";
+
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    // Default search term
+    this.onTermSubmit("pewdiepie");
+  }
+
+  onTermSubmit = async (term) => {
+    const res = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        type: "video",
+        key: KEY,
+      },
+    });
+    this.setState({ videos: res.data.items, selectedVideo: res.data.items[0] });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <Searchbar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Searchbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class Searchbar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+      e.preventDefault();
+      this.props.onFormSubmit(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="ui field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Searchbar;
